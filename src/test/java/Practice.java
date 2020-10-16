51f4c29cc8b88da18f51c5d4e6317acdf0dbc2c7
aytek <aytekbilici@gmail.com>
2020-10-16 15:55:15 +0200

    first change on the branch2

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -3,6 +3,7 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("It's going to first change on the branch");
         System.out.println("Let's start");
+        System.out.println("Changes on Branch2");
     }
 
 }
