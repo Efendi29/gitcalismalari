1e4cd18755ab5698b2948ca4a775212caf921a21
aytek <aytekbilici@gmail.com>
2020-10-16 15:50:20 +0200

    first and second change

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -2,5 +2,7 @@ public class Practice {
 
     public static void main(String[] args) {
         System.out.println("Let's start");
+        System.out.println("First change");
+        System.out.println("Second change");
     }
 }
